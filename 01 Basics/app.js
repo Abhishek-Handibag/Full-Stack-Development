eee48cf28720f10a202281a9c29c9160d21af003
Abhishek <abhishek.handibag12@gmail.com>
2024-08-04 00:27:09 +0530

    Comparison Operators

--- 01 Basics/app.js
+++ 01 Basics/app.js
@@ -8,3 +8,18 @@ console.log(name1);
 
 let number=55;
 console.log(typeof number); //used to check type of variables
+
+//comparison operators
+
+let num1=10;
+let num2=20;
+
+console.log(num1<num2);
+console.log(num1>num2);
+console.log(num1==num2); //only value
+console.log(1=='1'); // will be shown as true
+console.log(num1<=num2);
+console.log(num1>=num2);
+console.log(num1===num2); //strict equality type + value
+console.log(1==='1'); // it is false
+console.log(num1!=num2);
