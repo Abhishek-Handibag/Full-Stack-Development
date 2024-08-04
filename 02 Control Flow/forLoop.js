109397dc738f5a3cf5562b6bdc8ce131e357c5e0
Abhishek <abhishek.handibag12@gmail.com>
2024-08-04 10:55:14 +0530

    for loop added

---
+++ 02 Control Flow/forLoop.js
@@ -0,0 +1,13 @@
+let i;
+for (i = 0; i <= 25; i = i + 2) {
+  console.log(`The value of i is ${i}`);
+}
+
+// print your name & numbers from 0 to 1000
+
+let name="Abhishek";
+let num;
+
+for (num=0;num<=1000;num++){
+    console.log(`My name is ${name} and the number is ${num}`);
+}
