07ec69d16b88eb5b603e66f8a228dd72575147a3
Abhishek <abhishek.handibag12@gmail.com>
2024-08-04 10:47:40 +0530

    added swith example

---
+++ 02 Control Flow/switch.js
@@ -0,0 +1,15 @@
+let x=0;
+let text;
+switch(x){
+    case 0:
+        text = 'off';
+        break;
+    case 1:
+        text = 'on';
+        break;
+    
+    default:
+        text='nothing'
+        break;
+}
+console.log(text)
